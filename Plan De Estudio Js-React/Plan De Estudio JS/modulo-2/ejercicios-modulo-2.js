f1bca2d8b1bede9d6704fa78145ea355fcaf0352
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2025-05-04 16:35:46 -0500

    Agregar ejercicios del Módulo 2 y actualizar el script en index.html

---
+++ Plan De Estudio Js-React/Plan De Estudio JS/modulo-2/ejercicios-modulo-2.js
@@ -0,0 +1,38 @@
+// 🧪 Ejercicios del Módulo 2
+// 📌 Ejercicios base:
+// 1. Crea una función que reciba un número y diga si es par o impar.
+
+// let numero = parseFloat(prompt("Introduce un número: "));
+
+// function esParOImpar(numero) {
+//     if (numero % 2 === 0) {
+//         return "El número es par.";
+//     } else {
+//         return "El número es impar";
+//     }
+// }
+// let resultado = esParOImpar(numero);
+// console.log(resultado)
+
+// 2. Crea una función que reciba dos números y devuelva su promedio.
+
+// let numero1 = parseFloat(prompt("Introduce el primer número: "));
+// let numero2 = parseFloat(prompt("Introduce el segundo número: "));
+// function promedio(numero1, numero2) {
+//     return (numero1 + numero2) / 2;
+// }
+// let resultadoPromedio = promedio(numero1, numero2);
+// console.log("El promedio es: " + resultadoPromedio);
+
+// 3. Crea una función que reciba un texto y devuelva cuántas letras tiene.
+
+let texto = prompt("Introduce un texto: ");
+function contarLetras(texto) {
+    return texto.length;
+}
+let resultadoTexto = contarLetras(texto);
+console.log(`El texto tiene ${resultadoTexto} letras`)
+
+// 4. Crea una función que reciba un nombre y lo devuelva en mayúsculas.
+
+// 5. Crea una función que reciba el año de nacimiento y devuelva la edad.
