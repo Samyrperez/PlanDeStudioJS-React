641b3238a2e6ca8024f9ae9d38de5c10048cb096
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2025-05-13 15:29:16 -0500

    Agregar ejercicios de cuentas bancarias en JavaScript y tabla de operadores de asignación compuesta en el documento de métodos y funciones.

--- Plan De Estudio Js-React/Plan De Estudio JS/modulo-3/ejerciciosObjeto.js
+++ Plan De Estudio Js-React/Plan De Estudio JS/modulo-3/ejerciciosObjeto.js
@@ -25,3 +25,62 @@ Y los siguientes métodos:
 
 --Si intentas retirar más dinero del que hay, muestra: "Fondos insuficientes."
 */
+
+let cuentaBancaria= {
+    titular: "Samyr Perez",
+    saldo: 100000,
+    activa: true,
+    consultarSaldo: function() {
+        if (this.saldo === 0) {
+            console.log("No hay saldo en la cuenta")
+        } else {
+            console.log(`El saldo actual de la cuenta es de $${this.saldo}`)
+        }
+    },
+    depositar: function(cantidad) {
+        if (!this.activa) {
+            console.log("La cuenta está desactivada. No puedes realizar depósitos.");
+            return;
+        }
+    
+        if (typeof cantidad !== "number" || cantidad <= 0) {
+            console.log("Cantidad inválida. Ingresa un número positivo.");
+            return;
+        }
+    
+        this.saldo += cantidad; // Es lo mismo que : this.saldo = this.saldo + cantidad;
+        console.log(`Depósito exitoso. Nuevo saldo: $${this.saldo}`);
+    },
+    retirar: function(cantidad) {
+        if(!this.activa) {
+            console.log("La cuenta está desactivada. No puedes realizar retiros.");
+            return;
+        } 
+        if(typeof cantidad !== "number" || cantidad <= 0) {
+            console.log("Cantidad inválida. Ingresa un número positivo.");
+            return;
+        }
+        this.saldo -= cantidad;
+        console.log("Retiro exitoso. Nuevo saldo: $", this.saldo);
+    },
+    desactivarActivarCuenta: function(){
+        if(this.activa === true) {
+            this.activa = false;
+            console.log("Cuenta desactivada")
+            return;
+        } else {
+            this.activa = true;
+            console.log("Cuenta activada")
+        }
+    },
+    mostrarResumen: function(){
+        console.log(`Cuenta bancaria: \n Titular:${this.titular} \n Saldo Actual: ${this.saldo} \n Activa: ${this.activa ? "Activa": "Desactivada" }  `)
+    }
+}
+cuentaBancaria.consultarSaldo();
+cuentaBancaria.depositar(500);
+cuentaBancaria.retirar(100);
+cuentaBancaria.desactivarActivarCuenta()
+cuentaBancaria.desactivarActivarCuenta()
+cuentaBancaria.mostrarResumen();
+// console.log(cuentaBancaria);
